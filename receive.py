e335c070b8e7ae4f71427cfde395bfbe9eb12662
mark <mark3049@gmail.com>
2024-09-19 20:52:13 +0800

    add arduino receiver

---
+++ receive.py
@@ -0,0 +1,31 @@
+import serial
+import time
+import logging
+
+log = logging.getLogger(__name__)
+
+def read_serial(port):
+    if not port.readable():
+        return None
+    
+    line = port.readline().decode("utf-8").rstrip()
+    log.debug("read:%s", line)
+    if "waiting" in line:
+        return None
+    values = line.split(",")
+    if len(values) != 4:
+        return None
+    return [int(x) for x in values]
+    
+    
+
+if __name__ == "__main__":
+    logging.basicConfig(level = logging.INFO)
+    port = serial.Serial('/dev/ttyUSB0',9600)
+    while True:
+        time.sleep(1)
+        v = read_serial(port)
+        if v is None:
+            continue
+        log.info("%s", v)
+
