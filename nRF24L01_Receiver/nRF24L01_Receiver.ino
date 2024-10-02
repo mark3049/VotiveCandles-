708ea81b708e3e19925c571eeed93118c6448332
Mark.Chung <Mark.Chung@ampacscorp.com>
2024-10-02 16:19:29 +0800

    Change Arduino code

--- nRF24L01_Receiver/nRF24L01_Receiver.ino
+++ nRF24L01_Receiver/nRF24L01_Receiver.ino
@@ -7,20 +7,22 @@ RF24 radio(7, 8); //指定 Arduino Nano 腳位對應 nRF24L01 之 (CE, CSN)
 const byte address[6] = "65841";  //節點位址為 5 bytes + \0=6 bytes
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   radio.begin();  //初始化 nRF24L01 模組
   //printf_begin();  //初始化 RF24 的列印輸出功能
   radio.openReadingPipe(0, address);  //開啟 pipe 0 之讀取管線
-  radio.setPALevel(RF24_PA_MIN);  //設為低功率, 預設為 RF24_PA_MAX
+  radio.setPALevel(RF24_PA_LOW);  //設為低功率, 預設為 RF24_PA_MAX
   radio.startListening();  //接收端開始接收
   //radio.printDetails();  //印出 nRF24L01 詳細狀態
   //Serial.println("NRF24L01 receiver");
   Serial.println("waiting...");
+  //delay(1000);
 }
 
 void loop() {
 if (radio.available()) {  //偵測接收緩衝器是否有資料
     char text[32] = "";   //用來儲存接收字元之陣列
+    memset(text, 0 ,sizeof(text));
     radio.read(&text, sizeof(text));  //讀取接收字元
     Serial.println(text);
   }
--- nRF24L01_Sender/nRF24L01_Sender.ino
+++ nRF24L01_Sender/nRF24L01_Sender.ino
@@ -3,40 +3,38 @@
 
 RF24 radio(7, 8); //指定 Arduino Nano 腳位對應 nRF24L01 之 (CE, CSN)
 const byte address[6] = "65841";  //節點位址為 5 bytes + \0=6 bytes
-int silent_count;
+int silent_count = 0;
 
 void setup() {
   Serial.begin(9600);
   radio.begin();  //初始化 nRF24L01 模組
   radio.openWritingPipe(address);  //開啟寫入管線
-  radio.setPALevel(RF24_PA_MIN);   //設為低功率, 預設為 RF24_PA_MAX
+  radio.setPALevel(RF24_PA_MAX);   //設為低功率, 預設為 RF24_PA_MAX
   radio.stopListening();  //傳送端不需接收, 停止傾聽  // put your setup code here, to run once:
   silent_count = 0;
 }
 
-bool send_report(int v1, int v2, int v3, int v4)
-{
-  const char text[12];
-  sprintf(text, "%d,%d,%d,%d", v1, v2, v3, v4);
-  Serial.println(text);
-  radio.write(&text, sizeof(text));   //將字串寫入傳送緩衝器
-}
-
 void loop() {
   int v1,v2,v3,v4;
+  const char text[12];
+
   v1 = map(analogRead(A2), 0, 1024, 0,99);
   v2 = map(analogRead(A3), 0, 1024, 0,99);
   v3 = map(analogRead(A4), 0, 1024, 0,99);
   v4 = map(analogRead(A5), 0, 1024, 0,99);
-  if(v1 == 0 && v2 == 0 && v3 == 0 && v4 ==0){
+  sprintf(text, "%d,%d,%d,%d", v1, v2, v3, v4);
+
+  if(v1 == 0 && v2 == 0 && v3 == 0 && v4 == 0){
     ++silent_count;
     if(silent_count > 10){
       silent_count = 0;
-      send_report(v1, v2, v3, v4);
+      Serial.println(text);  
+      radio.write(&text, sizeof(text));   //將字串寫入傳送緩衝器
     }
   }else{
     silent_count = 10;
-    send_report(v1, v2, v3, v4);
+    Serial.println(text);
+    radio.write(&text, sizeof(text));   //將字串寫入傳送緩衝器
   }
   delay(1000);
 }
